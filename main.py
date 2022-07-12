591d01c3e460f61cf2d76a8744043684da819580
TJ Kapil <78827857+TJKAPILCODER@users.noreply.github.com>
2022-07-12 02:30:13 +0000

    PROGRAM MADE

--- main.py
+++ main.py
@@ -1,29 +1,45 @@
-print('''
-*******************************************************************************
-          |                   |                  |                     |
- _________|________________.=""_;=.______________|_____________________|_______
-|                   |  ,-"_,=""     `"=.|                  |
-|___________________|__"=._o`"-._        `"=.______________|___________________
-          |                `"=._o`"=._      _`"=._                     |
- _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
-|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
-|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
-          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
- _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
-|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
-|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
-____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
-/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
-____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
-/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
-____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
-/______/______/______/______/______/______/______/______/______/______/_____ /
-*******************************************************************************
-''')
-print("Welcome to Treasure Island.")
-print("Your mission is to find the treasure.") 
-
-#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
-
 #Write your code below this line 👇
+print("Welcome to the Spaceship!")
+print("Your mission is to find the hidden weapon. ")
+
+# PART 1 LEFT OR RIGHT DOOR
+
+door = input("You enter a ship, you notice two doors. Do You choose the left door or the right door? ")
+# Convert to lower case
+door = door.lower()
+# Your good if door is left. Other wise you die. 
+if door == "left":
+    # MOVE ON TO NEXT QUESTION (wait == good)
+    pool = input("You are standing infront of a pool with a bunch of unknown fish. Do you swim or do you wait? ")
+    # CONVERT TO LOWER
+    pool = pool.lower()
+    # WAIT == good
+    if pool == "wait":
+    # MOVE ON TO NEXT QUESTION
+        tunnel = input("You are in front of three tunnels. Do you choose the red, blue, or yellow tunnel? ")
+        # CONVERT TO LOWER
+        tunnel = tunnel.lower()
+        if tunnel == "yellow":
+            print("You WIN!")
+        elif tunnel == "red":
+            print("Burned by fire. Game Over.")
+        elif tunnel == "Blue":
+            print("Eaten by aliens. Game Over.")
+        else:
+            print("GAME OVER!!")
+    else:
+        print("Attacked by shark. Game Over.")
+               
+else:
+    print("You were burned by lasers. GAME OVER.")
+
+        
+
+
+    
+
+    
+    
+
+
 
